1829de625c4379cb5e645bea277ad7068f5c37f1
Soumya Manna <drasec.04@gmail.com>
2022-12-04 09:32:13 +0530

    PortFolio

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navlist.classList.remove('open');
 };
+
+
+
+// window.addEventListener("load", () => {
+//     const loader = document.querySelector(".loader");
+//     loader.classList.add("loader--hidden");
+
+//     loader.addEventListener("transitionend", () => {
+//         document.body.removeChild(loader);
+//     });
+// });
